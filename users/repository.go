a459b409dd87287b431dcab6c9db68b297199e3b
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-06-11 11:39:43 +0700

    repository update user

--- users/repository.go
+++ users/repository.go
@@ -7,6 +7,8 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindEmail(email string) (User, error)
+	FindById(id int) (User, error)
+	Update(user User) (User, error)
 }
 
 // struct respository untuk menampung koneksi database
@@ -39,5 +41,28 @@ func (r *repository) FindEmail(email string) (User, error) {
 		return user, err
 	}
 
+	return user, nil
+}
+
+// function untuk mencari data user berdasarkan ID
+func (r *repository) FindById(id int) (User, error) {
+	// inisiasi struct user sebagai nilai balikan
+	var user User
+
+	// cari id di database
+	err := r.db.Where("id = ?", id).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+// function untuk update user
+func (r *repository) Update(user User) (User, error) {
+	err := r.db.Save(&user).Error
+	if err != nil {
+		return user, err
+	}
 	return user, nil
 }
