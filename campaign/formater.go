3ddc201e9b6ba3c662ba9712c48cea06fb45c092
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-06-29 21:34:31 +0700

    add

--- campaign/formater.go
+++ campaign/formater.go
@@ -7,6 +7,7 @@ type CampaignFormatter struct {
 	ShortDescription string `json:"short_description"`
 	ImageURL         string `json:"image_url"`
 	GoalAmount       int    `json:"goal_amount"`
+	Slug             string `json:"slug"`
 	CurrentAmount    int    `json:"current_amount"`
 }
 
@@ -19,6 +20,7 @@ func FormatCampaign(campaign Campaign) CampaignFormatter {
 	campaignFormatter.ShortDescription = campaign.ShortDescription
 	campaignFormatter.CurrentAmount = campaign.CurrentAmount
 	campaignFormatter.GoalAmount = campaign.GoalAmount
+	campaignFormatter.Slug = campaign.Slug
 	campaignFormatter.ImageURL = ""
 
 	if len(campaign.CampaignImages) > 0 {
