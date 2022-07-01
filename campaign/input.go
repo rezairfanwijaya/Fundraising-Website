3c478136621511d57d73b8275e20c81b9b90f559
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-02 00:32:14 +0700

    add: input create campaign

--- campaign/input.go
+++ campaign/input.go
@@ -1,5 +1,7 @@
 package campaign
 
+import user "github.com/rezairfanwijaya/Fundraising-Website/users"
+
 // struct input untuk mengambil data campaign berdasarkan id
 // nanti id akan ditaruh di url sebagai uri
 // contoh api/campaign/1
@@ -7,4 +9,14 @@ package campaign
 
 type InputCampaignDetail struct {
 	Id int `uri:"id" binding:"required"`
+}
+
+// struct input create campaign
+type CreateCampaignInput struct {
+	Name             string `json:"name" binding:"required"`
+	ShortDescription string `json:"short_description" binding:"required"`
+	Description      string `json:"description" binding:"required"`
+	GoalAmount       int    `json:"goal_amount" binding:"required"`
+	Perks            string `json:"perks" binding:"required"`
+	User             user.User
 }
