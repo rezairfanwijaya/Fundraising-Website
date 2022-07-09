ba8e0a8154d9099e945e2c19cd1c8b201afe94b7
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-09 23:34:28 +0700

    add: transaction formatter

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -1,1 +1,33 @@
 package transaction
+
+import "time"
+
+// struct format response campaign transaction
+type CamapaignTransactionFormatter struct {
+	Id        int       `json:"id"`
+	Name      string    `json:"name"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// function untuk format single campaign transaction
+func FormatCampaignTransaction(transaction Transaction) CamapaignTransactionFormatter {
+	campaignTransactionFormatter := CamapaignTransactionFormatter{}
+	campaignTransactionFormatter.Id = transaction.Id
+	campaignTransactionFormatter.Name = transaction.User.Name
+	campaignTransactionFormatter.Amount = transaction.Amount
+	campaignTransactionFormatter.CreatedAt = transaction.CreatedAt
+
+	return campaignTransactionFormatter
+}
+
+// function untuk format many campaign transaction
+func FormatCampaignTransactions(transactions []Transaction) []CamapaignTransactionFormatter {
+	campaignTransactions := []CamapaignTransactionFormatter{}
+	for _, transaction := range transactions {
+		campaignTransaction := FormatCampaignTransaction(transaction)
+		campaignTransactions = append(campaignTransactions, campaignTransaction)
+	}
+
+	return campaignTransactions
+}
