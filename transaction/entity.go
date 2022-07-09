413a125435b21ce7d7e8f3532739d336da935399
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-09 21:44:20 +0700

    add: entity transaction

--- transaction/entity.go
+++ transaction/entity.go
@@ -1,1 +1,15 @@
 package transaction
+
+import "time"
+
+// struct models table transaction
+type Transaction struct {
+	Id         int
+	CampaignID int
+	UserId     int
+	Amount     int
+	Status     string
+	Code       string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
