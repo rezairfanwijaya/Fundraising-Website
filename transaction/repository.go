31d20e66061106798fd477842131a22a5513407a
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-09 23:32:25 +0700

    add: transaction repository

--- transaction/repository.go
+++ transaction/repository.go
@@ -1,1 +1,32 @@
 package transaction
+
+import "gorm.io/gorm"
+
+// bikin kontrak
+type Repository interface {
+	GetByCampaignId(campaignId int) ([]Transaction, error)
+}
+
+// bikin internal struct
+type repository struct {
+	db *gorm.DB
+}
+
+// bikin function new repo untuk dipakai di main
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+// function untuk mencari transaksi pada campaign tertentu
+func (r *repository) GetByCampaignId(campaignId int) ([]Transaction, error) {
+	// definisi return
+	var transaction []Transaction
+
+	// query
+	err := r.db.Preload("User").Where("campaign_id = ?", campaignId).Order("id desc").Find(&transaction).Error
+	if err != nil {
+		return transaction, err
+	}
+
+	return transaction, nil
+}
