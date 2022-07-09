4f35848b9de0fb7fe283e52f59a109e86420982a
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-09 23:33:31 +0700

    add: transaction service

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,45 @@
 package transaction
+
+import (
+	"errors"
+
+	"github.com/rezairfanwijaya/Fundraising-Website/campaign"
+)
+
+// bikin kontrak
+type Service interface {
+	GetTransactionByCampaignId(input GetTransactionsCampaignInput) ([]Transaction, error)
+}
+
+// bikin internal struct untuk meletakan dependensi
+type service struct {
+	repository   Repository
+	campaignRepo campaign.Repository
+}
+
+// bikin new service untuk dipakai di main
+func NewService(repository Repository, campaignRepo campaign.Repository) *service {
+	return &service{repository, campaignRepo}
+}
+
+// function untuk mengambil data transaksi berdasarkan campaign id
+func (s *service) GetTransactionByCampaignId(input GetTransactionsCampaignInput) ([]Transaction, error) {
+	// ambil campaign berdasarkan id
+	campaign, err := s.campaignRepo.FindById(input.CampaignId)
+	if err != nil {
+		return []Transaction{}, errors.New("campaign not found")
+	}
+
+	// cek apakah yang melakukan request adalah user pemilik campaign
+	if input.User.Id != campaign.UserId {
+		return []Transaction{}, errors.New("access denied, only owner campaign can access")
+	}
+
+	// panggil repo
+	transactions, err := s.repository.GetByCampaignId(input.CampaignId)
+	if err != nil {
+		return transactions, errors.New("transaction not found")
+	}
+
+	return transactions, nil
+}
