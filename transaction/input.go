23975b158bd2b4aa28629381ccf36f9323985db9
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-07-09 23:33:54 +0700

    add: transaction input

--- transaction/input.go
+++ transaction/input.go
@@ -1,1 +1,9 @@
 package transaction
+
+import user "github.com/rezairfanwijaya/Fundraising-Website/users"
+
+// struct input untuk menampilkan transaksi berdasrkan campaign id
+type GetTransactionsCampaignInput struct {
+	CampaignId int `uri:"id" binding:"required"`
+	User       user.User
+}
