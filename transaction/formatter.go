0c13305b3f192d6bad3055f3c0e72425c0b5faea
Reza Irfan Wijaya <reza.irfan@mekari.com>
2022-07-16 18:01:35 +0700

    add: formatter user transactions

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -2,7 +2,7 @@ package transaction
 
 import "time"
 
-// struct format response campaign transaction
+// struct format response campaign transaction by campaign id
 type CamapaignTransactionFormatter struct {
 	Id        int       `json:"id"`
 	Name      string    `json:"name"`
@@ -10,7 +10,21 @@ type CamapaignTransactionFormatter struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-// function untuk format single campaign transaction
+// stuct format response campaign transactin by user id
+type UserTransactionFormatter struct {
+	Id int `json:"id"`
+	Amount int `json:"amount"`
+	Status string `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+	Campaign CampaingFormatter `json:"campaign"` 
+}
+
+type CampaingFormatter struct {
+	Name string `json:"name"`
+	ImageURL string `json:"image_url"`
+}
+
+// function untuk format single campaign transaction transaction by campaign id
 func FormatCampaignTransaction(transaction Transaction) CamapaignTransactionFormatter {
 	campaignTransactionFormatter := CamapaignTransactionFormatter{}
 	campaignTransactionFormatter.Id = transaction.Id
@@ -31,3 +45,43 @@ func FormatCampaignTransactions(transactions []Transaction) []CamapaignTransacti
 
 	return campaignTransactions
 }
+
+// function untuk format single campaing transaction by user id
+func FormatUserTransaction(transactions Transaction) UserTransactionFormatter {
+	userTransactionFormatter := UserTransactionFormatter{}
+	userTransactionFormatter.Id = transactions.Id
+	userTransactionFormatter.Amount = transactions.Amount
+	userTransactionFormatter.Status = transactions.Status
+	userTransactionFormatter.CreatedAt = transactions.CreatedAt
+
+	campaign := CampaingFormatter{}
+	campaign.Name = transactions.Campaign.Name
+	campaign.ImageURL = ""
+	if len(transactions.Campaign.CampaignImages) > 0 {
+		campaign.ImageURL = transactions.Campaign.CampaignImages[0].FileName
+	}
+
+	userTransactionFormatter.Campaign = campaign
+
+	return userTransactionFormatter
+}
+
+// function untuk format many campaign transaction by user id
+ func FormatUserTransactions(transactions []Transaction) []UserTransactionFormatter {
+	// definisi return
+	userTransactionsFormatter := []UserTransactionFormatter{}
+
+	// cek apakah ada data 
+	if len(transactions) == 0 {
+		return userTransactionsFormatter
+	}
+
+	// lakukan format
+	for _, transaction := range transactions {
+		formatUserTransaction := FormatUserTransaction(transaction)
+		userTransactionsFormatter = append(userTransactionsFormatter, formatUserTransaction)
+	}
+
+	// return
+	return userTransactionsFormatter
+ }
