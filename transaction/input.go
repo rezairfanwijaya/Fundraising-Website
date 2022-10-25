b8349216df307eec70bf9a8a7145bace161c571c
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-10-25 22:08:09 +0700

    add: payload to get response payment notification

--- transaction/input.go
+++ transaction/input.go
@@ -14,3 +14,12 @@ type CreateTransactionInput struct {
 	CampaignId int       `json:"campaign_id" binding:"required"`
 	User       user.User `json:"user" binding:"required"`
 }
+
+// struct untuk handle notifikasi yang dikirim oleh midtrans
+// https://docs.midtrans.com/en/after-payment/http-notification
+type MidtransNotifications struct {
+	TransactionStatus string `json:"transaction_status"`
+	FraudStatus       string `json:"fraud_status"`
+	OrderId           string `json:"order_id"`
+	PaymentType       string `json:"payment_type"`
+}
